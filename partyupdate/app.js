6251668a2bb92a3be3b2e7b989e13a745e181694
Jay <88464412+jaythemountain@users.noreply.github.com>
2022-07-29 20:17:37 +0930

    Add files via upload

---
+++ partyupdate/app.js
@@ -0,0 +1,79 @@
+// name
+var nickname = prompt("May I please know your name", "Guest");
+if (nickname != null) {
+    document.getElementById("welcome").innerText = nickname;
+} 
+
+// greet
+const time = new Date().getHours();
+let greeting;
+if (time < 12) {
+  greeting = "Morning & Hi,";
+} else if (time < 17) {
+  greeting = "Afternoon & Hi,";
+} else {
+  greeting = "Evening & Hi,";
+}
+document.getElementById("greet").innerHTML = greeting;
+
+function random(num) {
+  return Math.floor(Math.random()*num)
+}
+
+function getRandomStyles() {
+  var r = random(255);
+  var g = random(255);
+  var b = random(255);
+  var mt = random(200);
+  var ml = random(50);
+  var dur = random(15)+5;
+  return `
+  background-color: rgba(${r},${g},${b},0.7);
+  color: rgba(${r},${g},${b},0.7); 
+  box-shadow: inset -7px -3px 10px rgba(${r-10},${g-10},${b-10},0.7);
+  margin: ${mt}px 0 0 ${ml}px;
+  animation: float ${dur}s ease-in infinite
+  `
+}
+
+function createBalloons(num) {
+  var balloonContainer = document.getElementById("balloon-container")
+  for (var i = num; i > 0; i--) {
+  var balloon = document.createElement("div");
+  balloon.className = "balloon";
+  balloon.style.cssText = getRandomStyles();           balloonContainer.append(balloon);
+  }
+}
+
+window.onload = function() {
+  createBalloons(500);
+}
+
+var confettiPlayers = [];
+
+function makeItConfetti() {
+  var confetti = document.querySelectorAll('.confetti');
+  
+  if (!confetti[0].animate) {
+    return false;
+  }
+
+  for (var i = 0, len = confetti.length; i < len; ++i) {
+    var snowball = confetti[i];
+    snowball.innerHTML = '<div class="rotate"><div class="askew"></div></div>';
+    var scale = Math.random() * .8 + .2;
+    var player = snowball.animate([
+      { transform: 'translate3d(' + (i/len*100) + 'vw,0,0) scale(' + scale + ')', opacity: scale },
+      { transform: 'translate3d(' + (i/len*100 + 10) + 'vw,100vh,0) scale(' + scale + ')', opacity: 1 }
+    ], {
+      duration: Math.random() * 3000 + 3000,
+      iterations: Infinity,
+      delay: -(Math.random() * 5000)
+    });
+    
+    
+    confettiPlayers.push(player);
+  }
+}
+
+makeItConfetti();
