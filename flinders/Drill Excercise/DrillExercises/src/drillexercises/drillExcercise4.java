5b681749a10f56aa9314b0562037b93664c7dd49
Jay <jay30.au@gmail.com>
2022-08-23 11:27:05 +0930

    a

---
+++ flinders/Drill Excercise/DrillExercises/src/drillexercises/drillExcercise4.java
@@ -0,0 +1,20 @@
+package drillexercises;
+
+import java.util.Scanner;
+
+public class drillExcercise4 {
+    Scanner scan = new Scanner(System.in);
+//  1.
+    //int A, B, C;
+    //A = B;
+  //  B= 8 ;
+
+//  2.
+    int digit = scan.nextInt();
+    int score1, score2, score3;
+    score1 = scan.nextInt()
+
+//  3.
+//  4.
+
+}
